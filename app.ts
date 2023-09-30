886c2909ae19e7ccbb745702d11919095c12865a
Aditi Polkam <aditipolkam@gmail.com>
2023-09-30 17:10:21 +0530

    add: cors

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cookieParser from "cookie-parser"
-
+import cors from "cors";
 import connectDB from './src/config/connectDB';
 import userRoutes from "./src/routes/user.route"
 import postRouter from "./src/routes/post.route"
@@ -9,6 +9,9 @@ const app = express();
 const port = 3000;
 connectDB();
 
+
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
